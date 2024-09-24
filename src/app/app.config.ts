969abe24cfd60ea45521b1a9aee44b1b39e67586
Adrian Dąbek <adrian950113@gmail.com>
2024-09-24 19:13:38 +0200

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideFirebaseApp} from "@angular/fire/app";
+import {initializeApp} from "firebase/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: "AIzaSyCWMEirzqk3V848iMTtGDUB2VwHq2YAHo8",
+        authDomain: "collector-21cff.firebaseapp.com",
+        projectId: "collector-21cff",
+        storageBucket: "collector-21cff.appspot.com",
+        messagingSenderId: "641294473346",
+        appId: "1:641294473346:web:7a381920ea715df6d5bc9b"
+      })
+    ),
+    provideFirestore(() => getFirestore()),]
 };
